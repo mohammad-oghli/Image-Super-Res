fc8a74538f724955bb455cb71d618e238da3339a
Mohamad Oghli <mhd.sh.oghli@gmail.com>
2022-08-29 10:15:25 +0400

    image super resolution v1

---
+++ app.py
@@ -0,0 +1,105 @@
+import streamlit as st
+import os
+import time
+from pathlib import Path
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+# from IPython.display import HTML, FileLink
+# from IPython.display import Image as DisplayImage
+# from IPython.display import Pretty, ProgressBar, clear_output, display
+from openvino.inference_engine import IECore
+from PIL import Image
+from io import BytesIO
+from helper import load_image, write_text_on_image, convert_result_to_image, to_rgb, pil_to_bytes
+from model_Inference import do_inference
+
+# Global model configuration
+# Setting
+DEVICE = "CPU"
+# 1032: 4x superresolution, 1033: 3x superresolution
+MODEL_FILE = "model/single-image-super-resolution-1032.xml"
+model_name = os.path.basename(MODEL_FILE)
+model_xml_path = Path(MODEL_FILE).with_suffix(".xml")
+
+# Load the Superresolution Model
+ie = IECore()
+net = ie.read_network(model=str(model_xml_path))
+exec_net = ie.load_network(network=net, device_name=DEVICE)
+
+
+def cv_superresolution(image_source):
+    OUTPUT_PATH = Path("output/")
+    os.makedirs(str(OUTPUT_PATH), exist_ok=True)
+    full_image = load_image(image_source)
+    # Uncomment these lines to load a raw image as BGR
+    # import rawpy
+    # with rawpy.imread(IMAGE_PATH) as raw:
+    #     full_image = raw.postprocess()[:,:,(2,1,0)]
+
+    # plt.imshow(to_rgb(full_image))
+    # print(f"Showing full image with width {full_image.shape[1]} " f"and height {full_image.shape[0]}")
+    # plt.show()
+    return do_inference(full_image, exec_net)
+
+
+def st_ui():
+    '''
+    Render the User Interface of the application endpoints
+    '''
+    st.title("Image Super Resolution")
+    st.caption("Image Quality Enhancement")
+    st.info("Single Image Super Resolution (SISR) Implementation by Oghli")
+
+    st.sidebar.subheader("Upload image to enhance its quality")
+    full_image = st.sidebar.file_uploader("Upload image", type=["png", "jpg"],
+                                          accept_multiple_files=False, key=None, help="Image to enhance its quality")
+    gen_btn = st.sidebar.button("Generate")
+    col1, col2 = st.columns(2)
+    s_msg = st.empty()
+    if full_image:
+        s_msg = st.sidebar.success("Image uploaded successfully")
+    # with st.spinner("Loading Input Image ..."):
+        # if full_image:
+        #     col1.subheader("Original Image")
+        #     col1.image(full_image, use_column_width=True)
+        #     # content_img_size = (500, 500)
+    if gen_btn:
+        s_msg.empty()
+        if full_image:
+            # with st.spinner('Generating Super Resolution Image ...'):
+            full_bicubic_image, full_superresolution_image = cv_superresolution(full_image)
+            col1.subheader("Original Image")
+            col1.image(to_rgb(full_bicubic_image), use_column_width=True)
+            col2.subheader("SISR Image")
+            col2.image(to_rgb(full_superresolution_image), use_column_width=True)
+            byte_super_img = pil_to_bytes(to_rgb(full_superresolution_image))
+            st.download_button(label="Download Result", data=byte_super_img,
+                               file_name="superres_image_4x.jpeg", mime="image/jpeg")
+            st.header("Animated GIF Comparison")
+            image_super = write_text_on_image(image=to_rgb(full_superresolution_image), text="SUPER")
+            image_bicubic = write_text_on_image(image=to_rgb(full_bicubic_image), text="BICUBIC")
+
+            img_array = [image_bicubic, image_super]
+            image_gif = st.empty()
+            i = 0
+            while True:
+                image_gif.image(img_array[i])
+                i = not i
+                time.sleep(2)
+        else:
+            st.sidebar.error("Please choose input image.")
+
+
+if __name__ == "__main__":
+    # render the app using streamlit ui function
+    st_ui()
+    # image_source = Path("images/mass_effect.jpg")
+    # #image_source = "https://i.imgur.com/R5ovXDO.jpg"
+    # full_bicubic_image, full_superresolution_image = cv_superresolution(image_source)
+    # fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(30, 15))
+    # ax[0].imshow(to_rgb(full_bicubic_image))
+    # ax[1].imshow(to_rgb(full_superresolution_image))
+    # ax[0].set_title("Bicubic")
+    # ax[1].set_title("Superresolution")
+    # plt.show()
---
+++ helper.py
@@ -0,0 +1,97 @@
+import urllib.request
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+# from IPython.display import HTML, FileLink
+# from IPython.display import Image as DisplayImage
+# from IPython.display import Pretty, ProgressBar, clear_output, display
+from io import BytesIO
+
+
+
+def write_text_on_image(image: np.ndarray, text: str) -> np.ndarray:
+    """
+    Write the specified text in the top left corner of the image
+    as white text with a black border.
+
+    :param image: image as numpy arry with HWC shape, RGB or BGR
+    :param text: text to write
+    :return: image with written text, as numpy array
+    """
+    font = cv2.FONT_HERSHEY_PLAIN
+    org = (20, 20)
+    font_scale = 4
+    font_color = (255, 255, 255)
+    line_type = 1
+    font_thickness = 2
+    text_color_bg = (0, 0, 0)
+    x, y = org
+
+    image = cv2.UMat(image)
+    (text_w, text_h), _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+    result_im = cv2.rectangle(image, org, (x + text_w, y + text_h), text_color_bg, -1)
+
+    textim = cv2.putText(
+        result_im,
+        text,
+        (x, y + text_h + font_scale - 1),
+        font,
+        font_scale,
+        font_color,
+        font_thickness,
+        line_type,
+    )
+    return textim.get()
+
+
+def load_image(path: str) -> np.ndarray:
+    """
+    Loads an image from `path` and returns it as BGR numpy array.
+
+    :param path: path to an image filename or url
+    :return: image as numpy array, with BGR channel order
+    """
+    if type(path) != str:
+        uploaded_file = path
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype="uint8")
+        image = cv2.imdecode(file_bytes, -1)
+    else:
+        if path.startswith("http"):
+            # Set User-Agent to Mozilla because some websites block requests
+            # with User-Agent Python
+            request = urllib.request.Request(path, headers={"User-Agent": "Mozilla/5.0"})
+            response = urllib.request.urlopen(request)
+            array = np.asarray(bytearray(response.read()), dtype="uint8")
+            image = cv2.imdecode(array, -1)  # Loads the image as BGR
+        else:
+            image = cv2.imread(path)
+    return image
+
+
+def convert_result_to_image(result) -> np.ndarray:
+    """
+    Convert network result of floating point numbers to image with integer
+    values from 0-255. Values outside this range are clipped to 0 and 255.
+
+    :param result: a single superresolution network result in N,C,H,W shape
+    """
+    result = result.squeeze(0).transpose(1, 2, 0)
+    result *= 255
+    result[result < 0] = 0
+    result[result > 255] = 255
+    result = result.astype(np.uint8)
+    return result
+
+
+def pil_to_bytes(image):
+    pil_image = Image.fromarray(np.squeeze(image).astype(np.uint8))
+    buffer = BytesIO()
+    pil_image.save(buffer, format="JPEG")
+    byte_image = buffer.getvalue()
+    return byte_image
+
+def to_rgb(image_data) -> np.ndarray:
+    """
+    Convert image_data from BGR to RGB
+    """
+    return cv2.cvtColor(image_data, cv2.COLOR_BGR2RGB)
